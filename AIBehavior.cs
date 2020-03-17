5fef5364e12fac04832b52c107a3268d56f76b7c
b-t-burch <38330762+b-t-burch@users.noreply.github.com>
2020-03-17 15:59:00 -0400

    AIbehavior.cs
    Class for enemy ai and boss ai has been started but not much has been changed

---
+++ AIBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*This class will contain the behavior for the enemies seen within the game.
+ * "Duckman" ai Critter(s) will patrol the maze
+ * "Garfield" ai critter will have exclusive behavior as it is the end "boss" AI
+ */
+
+namespace ACFramework
+{
+    class AIBehavior
+    {
+        cCritter3Dcharacter controlledcritter;
+        //-----------------------------------AI Constructor------------------------------\\
+        public AIBehavior(cCritter3Dcharacter aiCritter)
+        {
+            controlledcritter = aiCritter;
+            
+            aiPatrol(controlledcritter);
+        }
+
+        public void aiPatrol(cCritter3Dcharacter duckman)
+        {
+
+        }
+    }
+}
